de9291d041c32ccd04c6a927822b86cfe5c985f1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-19 20:24:07 -0300

    sprint_3

--- finix/app/_layout.tsx
+++ finix/app/_layout.tsx
@@ -1,18 +1,28 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Stack } from "expo-router";
+import { Stack, useSegments } from "expo-router";
 import { Pressable } from "react-native";
 
 export default function Layout() {
+  const segments = useSegments();
+
+  // Pega o nome da tela atual (último segmento da rota)
+  const currentRoute = segments[segments.length - 1];
+
+  // Lista de telas que NÃO devem mostrar o header
+  const hiddenHeaderScreens = ["Principal", "Usuario", "SemLogin"];
+
+  const hideHeader = hiddenHeaderScreens.includes(currentRoute);
+
   return (
     <Stack
       screenOptions={({ navigation }) => ({
-        headerShown: true,
+        headerShown: !hideHeader, // só oculta nas telas específicas
         headerBackTitle: "",
         headerTitle: "",
         headerTintColor: "#d9d9d9",
         headerStyle: { backgroundColor: "#121212", height: 100 },
         headerLeft: ({ canGoBack }) =>
-          canGoBack ? (
+          canGoBack && !hideHeader ? (
             <Pressable
               onPress={() => navigation.goBack()}
               style={{
